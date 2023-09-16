2b8bbdeb2f0d6d8dc35fc777ef1d2446e4a78d37
William M Babij <81699643+LordDemonos@users.noreply.github.com>
2023-09-16 15:08:49 -0400

    Add files via upload

--- ConvertMp4ToGif.bat
+++ ConvertMp4ToGif.bat
@@ -10,19 +10,23 @@ set /p "MAX_SIZE_MB=Please enter the maximum file size in MB for conversion (e.g
 :: Convert the maximum file size to bytes (1MB = 1048576 bytes)
 set /a "MAX_SIZE=MAX_SIZE_MB*1048576"
 
+:: The for /r command is used to iterate over all .mp4 files in the specified directory and its subdirectories.
 :: Iterate over all .mp4 files in the directory and its subdirectories
 for /r "%DIRECTORY%" %%F in (*.mp4) do (
+    
+    :: The %%~zA modifier is used to get the size of the current file in bytes.
     :: Get the size of the current file
     for %%A in ("%%F") do set "FILE_SIZE=%%~zA"
     
     :: Check if the file size is less than the maximum size
     if !FILE_SIZE! lss !MAX_SIZE! (
+        :: The %%~dpF and %%~nF modifiers are used to get the full path and name of the current file without the extension, respectively.
         :: Get the full path of the current file without extension
         set "FILE_PATH=%%~dpF"
         set "FILE_NAME=%%~nF"
         
         :: Convert the .mp4 file to .gif
-        ffmpeg -i "%%F" -vf "fps=10,scale=320:-1" -c:v gif "!FILE_PATH!!FILE_NAME!.gif"
+        ffmpeg -i "%%F" -vf "fps=10,scale=640:-1" -c:v gif "!FILE_PATH!!FILE_NAME!.gif"
     )
 )
 
@@ -30,6 +34,5 @@ for /r "%DIRECTORY%" %%F in (*.mp4) do (
 endlocal
 
 
-:: The for /r command is used to iterate over all .mp4 files in the specified directory and its subdirectories.
-:: The %%~zA modifier is used to get the size of the current file in bytes.
-:: The %%~dpF and %%~nF modifiers are used to get the full path and name of the current file without the extension, respectively.
+
+
---
+++ FileOrganizer.bat
@@ -0,0 +1,101 @@
+@echo off
+setlocal enabledelayedexpansion
+
+:: Prompt the user for the source directory
+set /p "SourceDir=Please enter the source directory: "
+
+:: Check if the directory exists
+if not exist "%SourceDir%" (
+    echo The directory does not exist.
+    exit /b
+)
+
+:: Initialize variables
+set "Password="
+set "RemoveArchives=0"
+set "KeepFile="
+
+:: Extract archive files in place using 7-Zip
+for %%F in (%SourceDir%\*.zip, %SourceDir%\*.rar, %SourceDir%\*.7z, %SourceDir%\*.tar.gz, %SourceDir%\*.jar, %SourceDir%\*.cab, %SourceDir%\*.dmg, %SourceDir%\*.kgb) do (
+    echo Extracting archive: %%~nxF
+    set "Password="
+    :PasswordLoop
+    if not defined Password (
+        set /p "Password=Enter password for archive %%~nxF (or press Enter if no password): "
+    )
+    "C:\Program Files\7-Zip\7z.exe" x "%%F" -o"%%~dpF" -p"!Password!"
+    if !errorlevel! neq 0 (
+        set "Password="
+        set /p "UserInput=Invalid password. Do you want to mark it for removal or keep it? (m/k): "
+        if /i "!UserInput!"=="m" (
+            echo Marking archive for removal: %%~nxF
+            echo Y | del "%%F" > nul
+        ) else (
+            echo Keeping archive: %%~nxF
+            set "KeepFile=%%F"
+        )
+        goto :PasswordLoop
+    )
+)
+
+:: Move remaining archives to the recycling bin
+for %%F in (%SourceDir%\*.zip, %SourceDir%\*.rar, %SourceDir%\*.7z, %SourceDir%\*.tar.gz, %SourceDir%\*.jar, %SourceDir%\*.cab, %SourceDir%\*.dmg, %SourceDir%\*.kgb) do (
+    if not "%%F"=="!KeepFile!" (
+        echo Moving archive to recycling bin: %%~nxF
+        PowerShell -Command "[Microsoft.VisualBasic.FileIO.FileSystem]::DeleteFile('%%F', [Microsoft.VisualBasic.FileIO.UIOption]::OnlyErrorDialogs, [Microsoft.VisualBasic.FileIO.RecycleOption]::SendToRecycleBin)"
+    )
+)
+
+:: Move all files from subdirectories to the source directory
+for /r "%SourceDir%" %%F in (*) do (
+    if not "%%~dpF"=="%SourceDir%\" (
+        echo Moving file: %%~nxF
+        move "%%F" "%SourceDir%"
+    )
+)
+
+:: Check and delete empty subdirectories
+set "TempFile=%TEMP%\EmptyDirs.txt"
+if exist "%TempFile%" del "%TempFile%"
+
+for /d /r "%SourceDir%" %%D in (*) do (
+    if "%%D" neq "%SourceDir%" (
+        dir /b "%%D" | findstr "^" >nul || (
+            echo Folder to delete: %%D
+            echo %%D >> "%TempFile%"
+        )
+    )
+)
+
+if exist "%TempFile%" (
+    type "%TempFile%"
+    set /p "UserInput=Do you want to delete all these folders? (y/n): "
+    if /i "!UserInput!"=="y" (
+        for /f "delims=" %%D in ('type "%TempFile%"') do (
+            rmdir "%%D"
+        )
+    )
+    del "%TempFile%"
+)
+
+:: Create Pictures subdirectory and move image files
+if not exist "%SourceDir%\Pictures" (
+    mkdir "%SourceDir%\Pictures"
+)
+for %%F in (%SourceDir%\*.jpg, %SourceDir%\*.jpeg, %SourceDir%\*.png, %SourceDir%\*.gif, %SourceDir%\*.bmp, %SourceDir%\*.tiff, %SourceDir%\*.webp, %SourceDir%\*.psd, %SourceDir%\*.raw, %SourceDir%\*.heif, %SourceDir%\*.indd, %SourceDir%\*.jpeg2000, %SourceDir%\*.svg, %SourceDir%\*.ai, %SourceDir%\*.eps, %SourceDir%\*.pdf) do (
+    echo Moving image file: %%~nxF
+    move "%%F" "%SourceDir%\Pictures"
+)
+
+:: Create Videos subdirectory and move video files and subtitles
+if not exist "%SourceDir%\Videos" (
+    mkdir "%SourceDir%\Videos"
+)
+for %%F in (%SourceDir%\*.webm, %SourceDir%\*.mpg, %SourceDir%\*.mp2, %SourceDir%\*.mpeg, %SourceDir%\*.mpe, %SourceDir%\*.mpv, %SourceDir%\*.ogg, %SourceDir%\*.mp4, %SourceDir%\*.m4p, %SourceDir%\*.m4a, %SourceDir%\*.m4v, %SourceDir%\*.avi, %SourceDir%\*.wmv, %SourceDir%\*.mov, %SourceDir%\*.qt, %SourceDir%\*.flv, %SourceDir%\*.swf, %SourceDir%\*.srt, %SourceDir%\*.sbv, %SourceDir%\*.ssa, %SourceDir%\*.ttml, %SourceDir%\*.dfxp, %SourceDir%\*.vtt, %SourceDir%\*avchd) do (
+    echo Moving video file: %%~nxF
+    move "%%F" "%SourceDir%\Videos"
+)
+
+
+echo Script completed.
+pause
