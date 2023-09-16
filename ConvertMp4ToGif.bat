a503eea9a47d7d8cc233c4e70ffe5e189d586080
William M Babij <81699643+LordDemonos@users.noreply.github.com>
2023-09-16 15:58:54 -0400

    Add files via upload

--- ConvertMp4ToGif.bat
+++ ConvertMp4ToGif.bat
@@ -7,8 +7,26 @@ set /p "DIRECTORY=Please enter the directory to search for .mp4 files: "
 :: Prompt the user to enter the maximum file size in MB for conversion
 set /p "MAX_SIZE_MB=Please enter the maximum file size in MB for conversion (e.g., 20): "
 
+:: Prompt the user to select GIF quality
+echo Select GIF quality:
+echo 1. 320px
+echo 2. 480px
+echo 3. 640px (default)
+echo 4. 800px
+echo 5. 1024px
+set /p "QUALITY=Enter the number corresponding to the desired quality: "
+
 :: Convert the maximum file size to bytes (1MB = 1048576 bytes)
 set /a "MAX_SIZE=MAX_SIZE_MB*1048576"
+
+:: Set the default scale value to 320
+set "SCALE=320"
+
+:: Update the scale value based on user's choice
+if %QUALITY% == 1 set "SCALE=320"
+if %QUALITY% == 2 set "SCALE=480"
+if %QUALITY% == 4 set "SCALE=800"
+if %QUALITY% == 5 set "SCALE=1024"
 
 :: The for /r command is used to iterate over all .mp4 files in the specified directory and its subdirectories.
 :: Iterate over all .mp4 files in the directory and its subdirectories
@@ -25,8 +43,8 @@ for /r "%DIRECTORY%" %%F in (*.mp4) do (
         set "FILE_PATH=%%~dpF"
         set "FILE_NAME=%%~nF"
         
-        :: Convert the .mp4 file to .gif
-        ffmpeg -i "%%F" -vf "fps=10,scale=640:-1" -c:v gif "!FILE_PATH!!FILE_NAME!.gif"
+        :: Convert the .mp4 file to .gif with the selected quality
+        ffmpeg -i "%%F" -vf "fps=10,scale=!SCALE!:-1" -c:v gif "!FILE_PATH!!FILE_NAME!.gif"
     )
 )
 
