351c1e2d5de872daf9a8796a1d1bfd4a5248b57f
William M Babij <81699643+LordDemonos@users.noreply.github.com>
2023-09-17 21:20:10 -0400

    Add files via upload

--- ConvertMp4ToGif.bat
+++ ConvertMp4ToGif.bat
@@ -1,6 +1,11 @@
 @echo off
 setlocal enabledelayedexpansion
 
+:: Initialize variables
+set "ERROR_COUNT=0"
+set "FILE_COUNT=0"
+set "TOTAL_SIZE=0"
+
 :: Prompt the user to enter the directory to start the search
 set /p "DIRECTORY=Please enter the directory to search for .mp4 files: "
 
@@ -36,7 +41,7 @@ if %QUALITY% == 3 set "SCALE=640"
 if %QUALITY% == 4 set "SCALE=800"
 if %QUALITY% == 5 set "SCALE=1024"
 
-:: Set the default frame rate to 15 fps
+:: Set the default frame rate to 10 fps
 set "FPS_VAL=10"
 
 :: Update the frame rate based on user's choice
@@ -44,30 +49,42 @@ if %FPS% == 1 set "FPS_VAL=10"
 if %FPS% == 2 set "FPS_VAL=15"
 if %FPS% == 3 set "FPS_VAL=30"
 
-:: The for /r command is used to iterate over all .mp4 files in the specified directory and its subdirectories.
 :: Iterate over all .mp4 files in the directory and its subdirectories
 for /r "%DIRECTORY%" %%F in (*.mp4) do (
     
-    :: The %%~zA modifier is used to get the size of the current file in bytes.
     :: Get the size of the current file
     for %%A in ("%%F") do set "FILE_SIZE=%%~zA"
     
     :: Check if the file size is less than the maximum size
     if !FILE_SIZE! lss !MAX_SIZE! (
-        :: The %%~dpF and %%~nF modifiers are used to get the full path and name of the current file without the extension, respectively.
+        
         :: Get the full path of the current file without extension
         set "FILE_PATH=%%~dpF"
         set "FILE_NAME=%%~nF"
         
         :: Convert the .mp4 file to .gif with the selected quality
- 	ffmpeg -y -i "%%F" -vf "fps=!FPS_VAL!,scale=!SCALE!:-1:flags=lanczos,split[v1][v2]; [v1]palettegen=stats_mode=full[palette]; [v2][palette]paletteuse=dither=sierra2_4a" -fps_mode passthrough "!FILE_PATH!!FILE_NAME!.gif"
-
+        ffmpeg -y -i "%%F" -vf "fps=!FPS_VAL!,scale=!SCALE!:-1:flags=lanczos,split[v1][v2]; [v1]palettegen=stats_mode=full[palette]; [v2][palette]paletteuse=dither=sierra2_4a" -fps_mode passthrough "!FILE_PATH!!FILE_NAME!.gif" 2>> error.log
+        IF %ERRORLEVEL% NEQ 0 (
+            echo An error occurred during conversion: %%F
+            set /a "ERROR_COUNT+=1"
+        ) else (
+            :: After creating the GIF, get its size in bytes and add it to the total
+            for %%G in ("!FILE_PATH!!FILE_NAME!.gif") do set /a "TOTAL_SIZE+=%%~zG"
+            
+            :: Increment the file count
+            set /a "FILE_COUNT+=1"
+        )
     )
 )
 
+:: Convert the total size to megabytes (1MB = 1048576 bytes)
+set /a "TOTAL_SIZE_MB=TOTAL_SIZE/1048576"
+
+:: Display a summary at the end
+echo.
+echo Script completed.
+echo Processed !FILE_COUNT! files totalling !TOTAL_SIZE_MB! MB with !ERROR_COUNT! errors.
+pause
+
 :end
 endlocal
-
-
-
-
